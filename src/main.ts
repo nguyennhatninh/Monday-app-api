f397bc3b8f6dd8937e3c983c631ac93ea90bbf48
Nhat Ninh <nguyennhatninh2212@gmail.com>
2024-07-27 17:46:50 +0700

    move folder middleware

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { loggerMiddleware } from 'middlewares';
+import { loggerMiddleware } from './middlewares';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
